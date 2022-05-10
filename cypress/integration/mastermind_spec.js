62ee04df2c0bb6142285c5f3b04bcb9385e6a10c
brittmagee <brittglynn@Brittanis-MacBook-Pro.local>
2022-05-10 14:29:09 -0400

    Changed add two item test file

--- cypress/integration/mastermind_spec.js
+++ cypress/integration/mastermind_spec.js
@@ -77,14 +77,14 @@ describe('Mastermind Tests', function(){
         cy.get(':nth-child(1) > .cart-sidebar-item__content > .quantity-selector > :nth-child(1) > span').click();
     });
 
-    it('Add two different items to cart', () => {
+    it.only('Add two different items to cart', () => {
         //Add two different items to cart and ensure price increments properly
         cy.get(':nth-child(5) > .product > .product-image > .product-qv > span').click({force: true});
         cy.get('.product-detail-content-one-atc > span').click();
         cy.get(':nth-child(13) > .product > .product-image > .product-qv > span').click({force: true});
         cy.get(':nth-child(2) > .ant-radio-button').click();
         cy.get(':nth-child(2) > .ant-radio-button > .ant-radio-button-input').check({force: true});
-        cy.get(':nth-child(18) > .ant-modal-root > .ant-modal-wrap > .ant-modal > .ant-modal-content > .ant-modal-body > .shop-qv > .ant-row > .ant-col-md-14 > .product-detail-content-one > .product-detail-content-one-actions > .product-detail-content-one-atc').click();
+        cy.get(':nth-child(18) > .ant-modal-root > .ant-modal-wrap > .ant-modal > .ant-modal-content > .ant-modal-body > .shop-qv > .ant-row > .ant-col-md-14 > .product-detail-content-one > .product-detail-content-one-actions > .product-detail-content-one-atc').click({force: true});
         cy.get('.menu-functions > :nth-child(3) > img').click();
         cy.get('.ant-drawer-close > .anticon > svg').click();
     });
