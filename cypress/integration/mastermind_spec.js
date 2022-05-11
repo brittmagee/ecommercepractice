04691ecbb95dfcc3a0543fece5fc7776afc732c8
brittmagee <brittglynn@Brittanis-MacBook-Pro.local>
2022-05-10 20:28:18 -0400

    Connected GitHub integration in Dashboard for flaky tests

--- cypress/integration/mastermind_spec.js
+++ cypress/integration/mastermind_spec.js
@@ -91,7 +91,7 @@ describe('Mastermind Tests', function(){
         cy.get(':nth-child(2) > .ant-radio-button > .ant-radio-button-input').check({force: true});
         cy.get(':nth-child(18) > .ant-modal-root > .ant-modal-wrap > .ant-modal > .ant-modal-content > .ant-modal-body > .shop-qv > .ant-row > .ant-col-md-14 > .product-detail-content-one > .product-detail-content-one-actions > .product-detail-content-one-atc > span').click({force: true});
         // cy.get('button').contains('Add to cart').click();
-        // cy.get('[data-cy="submit"]').click();
+        // cy.get('[data-cy="submit"]').click(); 
         cy.get('.menu-functions > :nth-child(3) > img').click();
         cy.get('.ant-drawer-close > .anticon > svg').click();
     });
