81fd07f45864c0b26d02849bfeb71c4e9086672a
brittmagee <brittglynn@Brittanis-MacBook-Pro.local>
2022-05-10 22:01:47 -0400

    Removing Chrome from default browser

--- cypress/integration/mastermind_spec.js
+++ cypress/integration/mastermind_spec.js
@@ -84,10 +84,8 @@ describe('Mastermind Tests', function(){
         cy.get(':nth-child(13) > .product > .product-image > .product-qv > span').click({force: true});
         cy.get(':nth-child(2) > .ant-radio-button').click();
         cy.get(':nth-child(2) > .ant-radio-button > .ant-radio-button-input').check({force: true});
-        cy.get(':nth-child(18) > .ant-modal-root > .ant-modal-wrap > .ant-modal > .ant-modal-content > .ant-modal-body > .shop-qv > .ant-row > .ant-col-md-14 > .product-detail-content-one > .product-detail-content-one-actions > .product-detail-content-one-atc > span', {timeout:10000}).click({force: true});
-        // cy.get('button').contains('Add to cart').click();
-        // cy.get('[data-cy="submit"]').click(); 
-        cy.get('.menu-functions > :nth-child(3) > img').click();
+        cy.get(':nth-child(18) > .ant-modal-root > .ant-modal-wrap > .ant-modal > .ant-modal-content > .ant-modal-body > .shop-qv > .ant-row > .ant-col-md-14 > .product-detail-content-one > .product-detail-content-one-actions > .product-detail-content-one-atc > span').click({force: true});
+        cy.get('.menu-functions > :nth-child(3) > img').click({force: true});
         cy.get('.ant-drawer-close > .anticon > svg').click();
     });
 
